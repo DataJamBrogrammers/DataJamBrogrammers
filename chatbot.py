458cf30124c65b09fe47216ae78a998780d8a818
Santiago <s.navarretev@uniandes.edu.co>
2024-05-08 21:49:55 -0500

    commit

--- chatbot.py
+++ chatbot.py
@@ -5,32 +5,14 @@ def handle(msg):
     print(msg)
     command=msg['text']
     if command=='salude':
-        bot.sendMessage(902264668, 'hola bebe')
-    if command=='soy crack':
+        bot.sendMessage(902264668, 'hola')
+    if command=='foto':
         bot.sendPhoto(902264668,photo=open('im1.jpg','rb'))
 
-def handle2(msg):
-    print(msg)
-    if 'text' in msg:
-        command=msg['text']
-        if command=='Hola':
-            bot.sendMessage(msg['from']['id'], 'Hola '+ msg['from']['first_name'] +' soy Bob, tu asistente virtual')
-            bot.sendMessage(msg['from']['id'], '¿En que puedo ayudarte?')
-            bot.sendMessage(msg['from']['id'], 'Manda una foto de tu problema para poder ayudarte')
-    else:
-        bot.sendMessage(msg['from']['id'], 'Foto recibida, en unos momentos te redigiré con los especialista adecuados')
-        bot.sendMessage(msg['from']['id'], 'Estoy procesando la imagen, por favor espera')
-        command=msg['photo'][2]['file_id']
-        bot.download_file(command, 'pruebaDuolan.jpg')
-        bot.sendMessage(msg['from']['id'], 'Parece que tu problema es una tubería rota, te redirigiré con los especialistas en plomería')
         
-
-
-
         
-# bot = telepot.Bot('1183214273:AAHgXQk-EXALGZRdQ5RM1klfUDxAp94gGcw') #NavsCrackBot
-bot = telepot.Bot('6814032942:AAHAB3RGrI5T6zMfPXE9C40Ehmhh_dhj6NI')
-MessageLoop(bot,handle2).run_forever()
+bot = telepot.Bot('7187218614:AAH8gD1WZBBCivg5yBYSFXRpSzfYZ0GNPG4')
+MessageLoop(bot,handle).run_forever()
 
 
 
